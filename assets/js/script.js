20fbf63ed79f3bf5a701b59c8a315d1212e169ea
Chael Santos <devin.chael@gmail.com>
2021-07-14 23:43:19 +0800

    Inner Page Updates

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+
+    $(document).scroll(function() {
+
+        var y = $(this).scrollTop();
+        //Scroll down 200 pixels to show navbar
+        if (y > 200) {
+        $('#navbar-main').addClass("navbar-scroll-bg");
+        } else {
+        $('#navbar-main').removeClass("navbar-scroll-bg");
+        }
+
+    });
+
+});
