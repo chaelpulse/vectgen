f03b2baafe4a851c6b45c79a201c8ddc43693028
Chael Santos <devin.chael@gmail.com>
2021-07-19 22:20:56 +0800

    Logo Update

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
 
         var y = $(this).scrollTop();
         //Scroll down 200 pixels to show navbar
-        if (y > 200) {
+        if (y > 50) {
         $('#navbar-main').addClass("navbar-scroll-bg");
         } else {
         $('#navbar-main').removeClass("navbar-scroll-bg");
